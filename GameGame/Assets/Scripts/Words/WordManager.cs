7d0d8cecfb64f59e17d0033f1d7b53c33814c788
Paisena <m8ames@gmail.com>
2026-07-13 16:36:15 -0700

    Feature: Setup word spawning timer
    created a timer to spawn the words

--- GameGame/Assets/Scripts/Words/WordManager.cs
+++ GameGame/Assets/Scripts/Words/WordManager.cs
@@ -1,18 +1,36 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WordManager : MonoBehaviour
 {
     [SerializeField] List<Word> WordList;  
+    [SerializeField] GameObject WordPrefab;
+    public bool CanSpawnWord = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(SpawnWordTimer());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnWordTimer()
+    {
+        do
+        {
+            SpawnWord();
+            yield return new WaitForSeconds(5);
+
+        } while (CanSpawnWord);
+    }
+
+    public void SpawnWord()
+    {
+        print("Spawning word");
     }
 }
