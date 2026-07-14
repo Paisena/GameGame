bff0459b2c12e802ec38cd4cbb97903f97e14a4b
Paisena <m8ames@gmail.com>
2026-07-14 00:07:40 -0700

    Feature: Added car movement
    left and right arrows move player position in new scene

---
+++ GameGame/Assets/Scripts/Car/CarController.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] List<Transform> CarPositions;
+    [SerializeField] int positionIndex = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPlayerInput();
+
+        UpdateCarPosition();        
+    }
+
+    private void CheckPlayerInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && positionIndex > 0)
+        {
+            print("moving player left");
+            positionIndex -= 1;
+        }    
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && positionIndex < CarPositions.Count-1)
+        {
+            print("moving player right");
+            positionIndex += 1;
+        }
+    }
+
+    private void UpdateCarPosition()
+    {
+        // will need to update to add animation later
+
+        gameObject.transform.position = CarPositions[positionIndex].position;
+    }
+
+}
